2c027ed7674fd2d0faf57d662178c849cabb96b0
AgustinDiaz14 <67121345+AgustinDiaz14@users.noreply.github.com>
2023-11-16 19:43:58 -0300

    assign a view function to marketplaceViewer.js

--- src/models/marketplaceViewer.js
+++ src/models/marketplaceViewer.js
@@ -1,8 +1,8 @@
-const MarketplaceViewerAbi = require("./../../abis/MarketplaceViewer.json")
 const addresses = require("./../../contractAddresses.json")
 const {getConfig} = require("../config/config");
+const MarketplaceAbi = require("../../abis/Marketplace.json");
 
-class MarketplaceViewer{
+class MarketplaceViewer {
 
     constructor() {
         this.config = getConfig()
@@ -12,50 +12,41 @@ class MarketplaceViewer{
         }
     }
 
+    async view(functionName, args) {
+        try {
+            return await this.config.publicClient.writeContract({
+                address: addresses.Marketplace.networks[this.config.networkId].address,
+                abi: MarketplaceAbi.abi,
+                functionName: functionName,
+                args: args
+            })
+        } catch (error) {
+            return error
+        }
+    }
+
     async getDeals(marketPlaceId, start, count, isProvider) {
         let deals = []
 
         let paginatorIndex = start
         let steps = count
+        let result = await this.view('getPaginatedDeals', [marketPlaceId, this.config.walletClient.account.address, isProvider, paginatorIndex, steps])
+        deals.push(...result[0])
 
-        try {
-            console.log("Address", addresses.MarketplaceViewer.networks[this.config.networkId], this.config.networkId)
-            let result = await this.config.publicClient.readContract({
-                address: addresses.MarketplaceViewer.networks[this.config.networkId].address,
-                abi: MarketplaceViewerAbi.abi,
-                functionName: 'getPaginatedDeals',
-                args: [marketPlaceId, this.config.walletClient.account.address, isProvider, paginatorIndex, steps]
-            })
-            console.log("Result", result)
-            deals.push(...result[0])
-
-            if (result[1] > deals.length) {
-                let totalDeals = result[1]
-                for (let i = 1; i * steps < totalDeals; i++) {
-                    let result = await this.config.publicClient.readContract({
-                        address: addresses.MarketplaceViewer[this.config.networkId],
-                        abi: MarketplaceViewerAbi.abi,
-                        functionName: 'getPaginatedDeals',
-                        args: [marketPlaceId, this.config.walletClient.account.address, isProvider, steps * i, steps]
-                    })
-                    deals.push(...result[0])
-                }
-
-                if (totalDeals > deals.length) {
-                    let result = await this.config.publicClient.readContract({
-                        address: addresses.MarketplaceViewer[this.config.networkId],
-                        abi: MarketplaceViewerAbi.abi,
-                        functionName: 'getPaginatedDeals',
-                        args: [marketPlaceId, this.config.walletClient.account.address, isProvider, deals.length, totalDeals - deals.length]
-                    })
-                    deals.push(...result[0])
-                }
+        if (result[1] > deals.length) {
+            let totalDeals = result[1]
+            for (let i = 1; i * steps < totalDeals; i++) {
+                let result = await this.view('getPaginatedDeals', [marketPlaceId, this.config.walletClient.account.address, isProvider, paginatorIndex + i * steps, steps])
+                deals.push(...result[0])
             }
 
-            return deals
-        } catch (error) {
-            return error
+            if (totalDeals > deals.length) {
+                let result = await this.view('getPaginatedDeals', [marketPlaceId, this.config.walletClient.account.address, isProvider, paginatorIndex + totalDeals, totalDeals - deals.length])
+                deals.push(...result[0])
+            }
         }
+
+        return deals
     }
 
 }
