f8f1510075daa695d2be0351d46fd7d9dfeacb71
AgustinDiaz14 <67121345+AgustinDiaz14@users.noreply.github.com>
2023-11-16 19:40:43 -0300

    remove redundant try catch on functions

--- src/models/marketplace.js
+++ src/models/marketplace.js
@@ -13,7 +13,7 @@ class Marketplace {
         }
     }
 
-    async execute(functionName, args){
+    async execute(functionName, args) {
         try {
             return await this.config.walletClient.writeContract({
                 address: addresses.Marketplace.networks[this.config.networkId].address,
@@ -27,83 +27,43 @@ class Marketplace {
     }
 
     async createOffer(marketPlaceId, maximumDeals, autoAccept, pricePerSecond, minDealDuration, billFullPeriods, singlePeriodOnly, metadata) {
-        try {
-            return await this.execute('createOffer', [marketPlaceId, maximumDeals, autoAccept, pricePerSecond, minDealDuration, billFullPeriods, singlePeriodOnly, metadata])
-        } catch (error) {
-            return error
-        }
+        return await this.execute('createOffer', [marketPlaceId, maximumDeals, autoAccept, pricePerSecond, minDealDuration, billFullPeriods, singlePeriodOnly, metadata])
     }
 
-    async updateOffer(marketPlaceId, offerId, maximumDeals, autoAccept, pricePerSecond, minDealDuration, billFullPeriod, singlePeriodOnly, metadata){
-        try {
-            return await this.execute('updateOffer', [marketPlaceId, offerId, maximumDeals, autoAccept, pricePerSecond, minDealDuration, billFullPeriod, singlePeriodOnly, metadata])
-        } catch (error) {
-            return error
-        }
+    async updateOffer(marketPlaceId, offerId, maximumDeals, autoAccept, pricePerSecond, minDealDuration, billFullPeriod, singlePeriodOnly, metadata) {
+        return await this.execute('updateOffer', [marketPlaceId, offerId, maximumDeals, autoAccept, pricePerSecond, minDealDuration, billFullPeriod, singlePeriodOnly, metadata])
     }
 
-    async deleteOffer(marketPlaceId, offerId){
-        try {
-            return await this.execute('deleteOffer', [marketPlaceId, offerId])
-        } catch (error) {
-            return error
-        }
+    async deleteOffer(marketPlaceId, offerId) {
+        return await this.execute('deleteOffer', [marketPlaceId, offerId])
     }
 
-    async createDeal(marketPlaceId, resourceId, offerId, blockedBalance, sharedKeyCopy){
-        try {
-            return await this.execute('createDeal', [marketPlaceId, resourceId, offerId, blockedBalance, sharedKeyCopy])
-        } catch (error) {
-            return error
-        }
+    async createDeal(marketPlaceId, resourceId, offerId, blockedBalance, sharedKeyCopy) {
+        return await this.execute('createDeal', [marketPlaceId, resourceId, offerId, blockedBalance, sharedKeyCopy])
     }
 
-    async createDeals(marketPlaceId, resourceId, offersId, blockedBalance, sharedKeyCopy){
-        try {
-            return await this.execute('createDeals', [marketPlaceId, resourceId, offersId, blockedBalance, sharedKeyCopy])
-        } catch (error) {
-            return error
-        }
+    async createDeals(marketPlaceId, resourceId, offersId, blockedBalance, sharedKeyCopy) {
+        return await this.execute('createDeals', [marketPlaceId, resourceId, offersId, blockedBalance, sharedKeyCopy])
     }
 
-    async acceptDeal(marketPlaceId, dealId){
-        try {
-            return await this.execute('acceptDeal', [marketPlaceId, dealId])
-        } catch (error) {
-            return error
-        }
+    async acceptDeal(marketPlaceId, dealId) {
+        return await this.execute('acceptDeal', [marketPlaceId, dealId])
     }
 
-    async rejectDeal(marketPlaceId, dealId){
-        try {
-            return await this.execute('rejectDeal', [marketPlaceId, dealId])
-        } catch (error) {
-            return error
-        }
+    async rejectDeal(marketPlaceId, dealId) {
+        return await this.execute('rejectDeal', [marketPlaceId, dealId])
     }
 
-    async cancelDeal(marketPlaceId, dealId){
-        try {
-            return await this.execute('cancelDeal', [marketPlaceId, dealId])
-        } catch (error) {
-            return error
-        }
+    async cancelDeal(marketPlaceId, dealId) {
+        return await this.execute('cancelDeal', [marketPlaceId, dealId])
     }
 
-    async cancelAllDeals(marketPlaceId, resourceId){
-        try {
-            return await this.execute('cancelAllDeals', [marketPlaceId, resourceId])
-        } catch (error) {
-            return error
-        }
+    async cancelAllDeals(marketPlaceId, resourceId) {
+        return await this.execute('cancelAllDeals', [marketPlaceId, resourceId])
     }
 
-    async initializeMarketplace(requiredStake, marketFeeTo, marketFeeRate){
-        try {
-            return await this.execute('initializeMarketplace', [requiredStake, marketFeeTo, marketFeeRate])
-        } catch (error) {
-            return error
-        }
+    async initializeMarketplace(requiredStake, marketFeeTo, marketFeeRate) {
+        return await this.execute('initializeMarketplace', [requiredStake, marketFeeTo, marketFeeRate])
     }
 
 }
