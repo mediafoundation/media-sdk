f43f091cb3378076a189a8d022c198a064cf989e
AgustinDiaz14 <67121345+AgustinDiaz14@users.noreply.github.com>
2023-11-17 18:48:56 -0300

    adding current block number util

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const {initSdk} = require("./src/config/config");
 const MarketplaceViewer = require("./src/models/marketplaceViewer");
 const Resources = require("./src/models/resources")
 const Encryption = require("./src/utils/encryption");
+const EventHandler = require("./src/models/eventsHandler");
+const Blockchain = require("./src/utils/blockchain");
 
 module.exports = {
     initSdk,
     Marketplace,
     MarketplaceViewer,
     Resources,
-    Encryption
+    Encryption,
+    EventHandler,
+    Blockchain,
 }
