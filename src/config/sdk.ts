e0aeb583734eff7cb47e16a05dc344aa9ffb0ac9
AgustinDiaz14 <67121345+AgustinDiaz14@users.noreply.github.com>
2024-07-31 20:35:34 -0300

    docs(sdk.ts): document entire file

--- src/config/sdk.ts
+++ src/config/sdk.ts
@@ -1,20 +1,6 @@
-import {baseSepolia, sepolia} from "viem/chains";
+import { mnemonicToAccount, privateKeyToAccount } from "viem/accounts";
 
-
-interface SdkConstructor {
-    chain?: Chain,
-    transport?: Transport[],
-    privateKey?: string | undefined,
-    mnemonic?: string | undefined,
-    walletClient?: any | undefined,
-}
-
-export type SdkConfig = {
-    walletClient: WalletClient,
-    publicClient: PublicClient
-}
-
-const defaultChain = sepolia
+import { baseSepolia, sepolia } from "viem/chains";
 
 import {
     Chain,
@@ -26,10 +12,37 @@ import {
     Transport,
     WalletClient
 } from "viem";
-import {mnemonicToAccount, privateKeyToAccount} from "viem/accounts";
 
+/**
+ * Interface for SDK constructor parameters.
+ */
+interface SdkConstructor {
+    chain?: Chain,
+    transport?: Transport[],
+    privateKey?: string | undefined,
+    mnemonic?: string | undefined,
+    walletClient?: any | undefined,
+}
+
+/**
+ * Type for SDK configuration.
+ */
+export type SdkConfig = {
+    walletClient: WalletClient,
+    publicClient: PublicClient
+}
+const defaultChain = sepolia
+
+/**
+ * SDK class for managing blockchain interactions.
+ */
 export class Sdk {
     public config: SdkConfig
+
+    /**
+     * Constructor for the SDK class.
+     * @param {SdkConstructor} param0 - The constructor parameters.
+     */
     constructor({
         chain = defaultChain,
         transport = undefined,
@@ -47,26 +60,42 @@ export class Sdk {
         }
         let publicClient = walletClient
             ? walletClient.extend(publicActions)
-            : this.generatePublicClient({chain, transports: transport || [http(chain?.rpcUrls.default.http[0])]})
+            : this.generatePublicClient({ chain, transports: transport || [http(chain?.rpcUrls.default.http[0])] })
         this.config = {
             walletClient: walletClient,
             publicClient: publicClient,
         }
     }
 
+    /**
+     * Generates a public client.
+     * @param {Object} param0 - The parameters for generating a public client.
+     * @param {Chain} param0.chain - The blockchain chain.
+     * @param {Transport[]} param0.transports - The transport methods.
+     * @returns {PublicClient} The public client.
+     */
     generatePublicClient({
         chain,
         transports
     }: {
         chain: Chain,
         transports: Transport[],
-    }) {
+    }): PublicClient {
         return createPublicClient({
             chain: chain,
             transport: fallback(transports),
         })
     }
 
+    /**
+     * Generates a wallet client.
+     * @param {Object} param0 - The parameters for generating a wallet client.
+     * @param {Chain} param0.chain - The blockchain chain.
+     * @param {Transport[]} param0.transports - The transport methods.
+     * @param {string | undefined} param0.privateKey - The private key.
+     * @param {string | undefined} param0.mnemonic - The mnemonic phrase.
+     * @returns {WalletClient} The wallet client.
+     */
     generateWalletClient({
         chain,
         transports,
@@ -77,7 +106,7 @@ export class Sdk {
         transports: Transport[],
         privateKey: string | undefined,
         mnemonic: string | undefined
-    }) {
+    }): WalletClient {
         let account = privateKey
             ? privateKeyToAccount(`0x${privateKey}`)
             : mnemonicToAccount(mnemonic!)
@@ -89,6 +118,9 @@ export class Sdk {
     }
 }
 
+/**
+ * Chains currently supported.
+ */
 export const validChains = {
     11155111: sepolia,
     84532: baseSepolia,
