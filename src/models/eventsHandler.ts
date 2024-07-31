a52be35c30400e1997c4f4a142d8c59ef27dcfdd
AgustinDiaz14 <67121345+AgustinDiaz14@users.noreply.github.com>
2024-07-31 20:46:35 -0300

    docs(eventsHandler.ts): Document entire file

--- src/models/eventsHandler.ts
+++ src/models/eventsHandler.ts
@@ -1,20 +1,36 @@
 import Addresses from "../../contractAddresses.json";
 
-import {Sdk, SdkConfig} from "../config/sdk";
-import {Address, Log} from "viem";
+import { Sdk, SdkConfig } from "../config/sdk";
+import { Address, Log } from "viem";
 import abis from "../../abis";
-import {ContractEvent, GetPastEventParams, ListenForEventsParams} from "../types/modelTypes";
+import { ContractEvent, GetPastEventParams, ListenForEventsParams } from "../types/modelTypes";
 const ContractAddresses: typeof Addresses = Addresses
 
-
-
+/**
+ * Class for handling events from various contracts.
+ */
 export class EventsHandler {
   private config: SdkConfig
 
+  /**
+   * Creates an instance of EventsHandler.
+   * @param {Sdk} sdkInstance - An instance of the SDK.
+   */
   constructor(sdkInstance: Sdk) {
     this.config = sdkInstance.config
   }
 
+  /**
+   * Retrieves past events from a specified contract.
+   * @param {Object} param0 - The parameters for retrieving past events.
+   * @param {string} param0.contractName - The name of the contract.
+   * @param {any} param0.contractAbi - The ABI of the contract.
+   * @param {string[]|string|undefined} [param0.eventName] - The name(s) of the event(s) to retrieve.
+   * @param {bigint} param0.fromBlock - The starting block number.
+   * @param {bigint} param0.toBlock - The ending block number.
+   * @returns {Promise<ContractEvent[]>} The past events.
+   * @throws Will throw an error if the contract address is not found for the network ID.
+   */
   async getPastEvents({
     contractName,
     contractAbi,
@@ -46,6 +62,16 @@ export class EventsHandler {
     }) as ContractEvent[]
   }
 
+  /**
+   * Listens for a specific event from a contract.
+   * @param {Object} param0 - The parameters for listening to contract events.
+   * @param {string} param0.contractName - The name of the contract.
+   * @param {any} param0.contractAbi - The ABI of the contract.
+   * @param {string} param0.eventName - The name of the event to listen for.
+   * @param {function} param0.callback - The callback function to handle the event logs.
+   * @param {function} param0.onError - The callback function to handle errors.
+   * @throws Will throw an error if the contract address is not found for the network ID.
+   */
   async listenForContractEvent({
     contractName,
     contractAbi,
@@ -77,6 +103,14 @@ export class EventsHandler {
     })
   }
 
+  /**
+   * Retrieves past events from the Marketplace contract.
+   * @param {Object} param0 - The parameters for retrieving past events.
+   * @param {string[]|string|undefined} [param0.eventName] - The name(s) of the event(s) to retrieve.
+   * @param {bigint} param0.fromBlock - The starting block number.
+   * @param {bigint} param0.toBlock - The ending block number.
+   * @returns {Promise<ContractEvent[]>} The past events.
+   */
   async getMarketplacePastEvents({ eventName, fromBlock, toBlock }: GetPastEventParams): Promise<ContractEvent[]> {
     return await this.getPastEvents({
       contractName: "Marketplace",
@@ -87,6 +121,14 @@ export class EventsHandler {
     })
   }
 
+  /**
+   * Retrieves past events from the MarketplaceViewer contract.
+   * @param {Object} param0 - The parameters for retrieving past events.
+   * @param {string[]|string|undefined} [param0.eventName] - The name(s) of the event(s) to retrieve.
+   * @param {bigint} param0.fromBlock - The starting block number.
+   * @param {bigint} param0.toBlock - The ending block number.
+   * @returns {Promise<ContractEvent[]>} The past events.
+   */
   async getMarketplaceViewerPastEvents({ eventName, fromBlock, toBlock }: GetPastEventParams): Promise<ContractEvent[]> {
     return await this.getPastEvents({
       contractName: "MarketplaceViewer",
@@ -97,6 +139,14 @@ export class EventsHandler {
     })
   }
 
+  /**
+   * Retrieves past events from the Resources contract.
+   * @param {Object} param0 - The parameters for retrieving past events.
+   * @param {string[]|string|undefined} [param0.eventName] - The name(s) of the event(s) to retrieve.
+   * @param {bigint} param0.fromBlock - The starting block number.
+   * @param {bigint} param0.toBlock - The ending block number.
+   * @returns {Promise<ContractEvent[]>} The past events.
+   */
   async getResourcesPastEvents({ eventName, fromBlock, toBlock }: GetPastEventParams): Promise<ContractEvent[]> {
     return await this.getPastEvents({
       contractName: "Resources",
@@ -107,6 +157,13 @@ export class EventsHandler {
     })
   }
 
+  /**
+   * Listens for a specific event from the Marketplace contract.
+   * @param {Object} param0 - The parameters for listening to contract events.
+   * @param {string} param0.eventName - The name of the event to listen for.
+   * @param {function} param0.callback - The callback function to handle the event logs.
+   * @param {function} param0.onError - The callback function to handle errors.
+   */
   async listenForMarketplaceEvent({ eventName, callback, onError }: ListenForEventsParams) {
     await this.listenForContractEvent({
       contractName: "Marketplace",
@@ -117,6 +174,13 @@ export class EventsHandler {
     })
   }
 
+  /**
+   * Listens for a specific event from the MarketplaceViewer contract.
+   * @param {Object} param0 - The parameters for listening to contract events.
+   * @param {string} param0.eventName - The name of the event to listen for.
+   * @param {function} param0.callback - The callback function to handle the event logs.
+   * @param {function} param0.onError - The callback function to handle errors.
+   */
   async listenForMarketplaceViewerEvent({ eventName, callback, onError }: ListenForEventsParams) {
     await this.listenForContractEvent({
       contractName: "MarketplaceViewer",
@@ -127,6 +191,13 @@ export class EventsHandler {
     })
   }
 
+  /**
+   * Listens for a specific event from the Resources contract.
+   * @param {Object} param0 - The parameters for listening to contract events.
+   * @param {string} param0.eventName - The name of the event to listen for.
+   * @param {function} param0.callback - The callback function to handle the event logs.
+   * @param {function} param0.onError - The callback function to handle errors.
+   */
   async listenForResourcesEvent({ eventName, callback, onError }: ListenForEventsParams) {
     await this.listenForContractEvent({
       contractName: "Resources",
